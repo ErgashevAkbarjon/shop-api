bbf4a344145c2eec35b737eb3f1466f7ddf82700
Ergashev Akbar <ergashev.akb@gmail.com>
2020-07-21 19:15:00 +0500

    Add support of mass assignment

--- app/Category.php
+++ app/Category.php
@@ -8,6 +8,8 @@ class Category extends Model
 {
     public $timestamps = false;
 
+    protected $guarded = [];
+
     public function products()
     {
         return $this->belongsToMany(Product::class);
--- app/Product.php
+++ app/Product.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $fillable = [
+        "name",
+        "image",
+        "price",
+        "published"
+    ];
+
     public function categories()
     {
         return $this->belongsToMany(Category::class);
