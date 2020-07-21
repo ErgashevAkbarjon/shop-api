dc02c9f9739af5a36e5414d619d370ad3c55cec7
Ergashev Akbar <ergashev.akb@gmail.com>
2020-07-21 18:59:48 +0500

    Made product image as nullable

--- database/migrations/2020_07_21_131643_create_products_table.php
+++ database/migrations/2020_07_21_131643_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->double('price');
             $table->boolean('published')->default(true);
             $table->timestamps();
